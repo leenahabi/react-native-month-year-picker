a1009788e7481e9cb3c53682a77887a3e544d213
leenahabi <50432005+leenahabi@users.noreply.github.com>
2023-08-03 09:50:51 +0300

    Update RNMonthPicker+Toolbar.m

--- ios/RNMonthPicker+Toolbar.m
+++ ios/RNMonthPicker+Toolbar.m
@@ -17,11 +17,12 @@ RNMonthPicker *picker;
 - (instancetype)initWithFrame:(CGRect)frame {
     CGRect screen = [[UIScreen mainScreen] bounds];
     if ((self = [super initWithFrame:frame])) {
-        toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(screen), 44)];
+(*         toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 0, CGRectGetWidth(screen), 44)]; *)
         cancelButton = [[UIBarButtonItem alloc] initWithTitle:@"Cancel" style:UIAlertActionStyleCancel target:self action:@selector(onCancelButton)];
         doneButton = [[UIBarButtonItem alloc] initWithTitle:@"Done" style:UIBarButtonItemStyleDone target:self action:@selector(onDoneButton)];
         picker = [RNMonthPicker new];
-        picker.frame = CGRectMake(0, 44, CGRectGetWidth(screen), 200);
+        CGFloat pickerWidth = 310;
+        picker.frame = CGRectMake((CGRectGetWidth(screen) - 350) / 2, 0, pickerWidth, 200);
         UIColor* defaultColor;
         if (@available(iOS 13.0, *)) {
             defaultColor = [UIColor tertiarySystemBackgroundColor];
@@ -29,7 +30,7 @@ RNMonthPicker *picker;
             defaultColor = [UIColor whiteColor];
         }
         [self setValue:defaultColor forKey:@"backgroundColor"];
-        [self addSubview:toolbar];
+(*         [self addSubview:toolbar]; *)
         [self addSubview:picker];
     }
     return self;
